727d81a0e99f3b1e4786b9046268dd29f06e89bb
Rahul <rgupta@argolimited.com>
2019-07-20 17:26:50 +0530

    aded perterr

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const http = require('http')
+const http = require('http');
 const fs = require('fs');
 
-const { sitemapGenerator } = require('./utils')
+const { sitemapGenerator } = require('./utils');
 const port = 3000 || process.env.port;
 console.log('TCL: process.env.port;', process.env.port);
 
 const app = http.createServer((req, res) => {
   if (req.method == 'POST') {
-    let string = ''
-    req.on('data', (chunk) => {
-      string += chunk.toString()
-    })
+    let string = '';
+    req.on('data', chunk => {
+      string += chunk.toString();
+    });
     req.on('end', () => {
       console.log('TCL: string', string);
-      const { url } = JSON.parse(string)
+      const { url } = JSON.parse(string);
       console.log('TCL: url', url);
 
       const fileName = sitemapGenerator(url);
       console.log('TCL: fileName', fileName);
       res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ 'response': url }));
-    })
+      res.end(JSON.stringify({ response: url }));
+    });
   }
-})
+});
 
-const server = app.listen(process.env.PORT, function () {
-  console.log("API listen at port 3000");
+const server = app.listen(process.env.PORT, function() {
+  console.log('API listen at port 3000');
 });
 
 module.exports = server;
--- utils.js
+++ utils.js
@@ -1,85 +1,84 @@
-// var request = require('request');
-// var cheerio = require('cheerio');
-// var URL = require('url-parse');
+var request = require('request');
+var cheerio = require('cheerio');
+var URL = require('url-parse');
 
-// // var START_URL = "http://www.arstechnica.com";
-// var START_URL = 'https://wiprodigital.com/'
-// // var START_URL = 'http://127.0.0.1:5500/'
-// var MAX_PAGES_TO_VISIT = 10;
+// var START_URL = "http://www.arstechnica.com";
+var START_URL = 'https://wiprodigital.com/';
+// var START_URL = 'http://127.0.0.1:5500/'
+var MAX_PAGES_TO_VISIT = 10;
 
-// var pagesVisited = {};
-// var numPagesVisited = 0;
-// var pagesToVisit = [];
-// var url = new URL(START_URL);
-// var baseUrl = url.protocol + "//" + url.hostname;
+var pagesVisited = {};
+var numPagesVisited = 0;
+var pagesToVisit = [];
+var url = new URL(START_URL);
+var baseUrl = url.protocol + '//' + url.hostname;
 
-// // var baseUrl = START_URL;   
+// var baseUrl = START_URL;
 
-// const finalResult = []
-// pagesToVisit.push(START_URL);
-// crawl();
+const finalResult = [];
+pagesToVisit.push(START_URL);
+crawl();
 
-// function crawl() {
-//   if (numPagesVisited >= MAX_PAGES_TO_VISIT) {
-//     console.log("Reached max limit of number of pages to visit.");
-//     console.log('finish', pagesVisited);
-//     console.log('need to visit', pagesToVisit)
-//     return;
-//   }
-//   console.log('TCL: crawl -> pagesToVisit', pagesToVisit.length);
-//   var nextPage = pagesToVisit.length && pagesToVisit.pop();
-//   console.log('TCL: crawl -> nextPage', nextPage);
-//   if (!!nextPage) {
-//     if (nextPage in pagesVisited) {
-//       // We've already visited this page, so repeat the crawl
-//       crawl();
-//     } else {
-//       // New page we haven't visited
-//       visitPage(nextPage, crawl);
-//     }
-//   } else {
-//     console.log('finish', pagesVisited);
-//   }
-// }
+function crawl() {
+  if (numPagesVisited >= MAX_PAGES_TO_VISIT) {
+    console.log('Reached max limit of number of pages to visit.');
+    console.log('finish', pagesVisited);
+    console.log('need to visit', pagesToVisit);
+    return;
+  }
+  console.log('TCL: crawl -> pagesToVisit', pagesToVisit.length);
+  var nextPage = pagesToVisit.length && pagesToVisit.pop();
+  console.log('TCL: crawl -> nextPage', nextPage);
+  if (!!nextPage) {
+    if (nextPage in pagesVisited) {
+      // We've already visited this page, so repeat the crawl
+      crawl();
+    } else {
+      // New page we haven't visited
+      visitPage(nextPage, crawl);
+    }
+  } else {
+    console.log('finish', pagesVisited);
+  }
+}
 
-// function visitPage(url, callback) {
-//   console.log('TCL: visitPage -> url', url);
-//   // Add page to our set
-//   pagesVisited[url] = true;
-//   numPagesVisited++;
+function visitPage(url, callback) {
+  console.log('TCL: visitPage -> url', url);
+  // Add page to our set
+  pagesVisited[url] = true;
+  numPagesVisited++;
 
-//   // Make the request
-//   console.log("Visiting page " + url);
-//   request(url, function (error, response, body) {
-//     // Check status code (200 is HTTP OK)
-//     console.log("Status code: " + response.statusCode);
-//     if (response.statusCode !== 200) {
-//       callback();
-//       return;
-//     }
-//     // Parse the document body
-//     var $ = cheerio.load(body);
+  // Make the request
+  console.log('Visiting page ' + url);
+  request(url, function(error, response, body) {
+    // Check status code (200 is HTTP OK)
+    console.log('Status code: ' + response.statusCode);
+    if (response.statusCode !== 200) {
+      callback();
+      return;
+    }
+    // Parse the document body
+    var $ = cheerio.load(body);
 
-//     collectInternalLinks($);
-//     // In this short program, our callback is just calling crawl()
-//     callback();
-//   });
-// }
+    collectInternalLinks($);
+    // In this short program, our callback is just calling crawl()
+    callback();
+  });
+}
 
+function collectInternalLinks($) {
+  const links = $('a');
+  console.log('Found ' + links.length + ' relative links on page');
+  $(links).each(function(i, link) {
+    const url = new URL($(link).attr('href'));
+    pagesToVisit.push(baseUrl + url.pathname);
+  });
+}
 
-// function collectInternalLinks($) {
-//   const links = $('a');
-//   console.log("Found " + links.length + " relative links on page");
-//   $(links).each(function (i, link) {
-//     const url = new URL($(link).attr('href'));
-//     pagesToVisit.push(baseUrl + url.pathname);
-//   });
-// }
-
-module.exports.sitemapGenerator = (url) => {
+module.exports.sitemapGenerator = url => {
   // const fileName = `${uniqueId}.json`
   // fs.writeFile(fileName, data, function (err) {
   //   if (err) { throw `error:${err}` }
   // });
-  return url
-}
+  return url;
+};
