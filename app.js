1fd0fcf33d8d2583147375eaae26860bf9eed2c1
Rahul <rgupta@argolimited.com>
2019-07-20 16:39:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const fs = require('fs');
+
+const { sitemapGenerator } = require('./utils')
+const port = 3000 || process.env.port;
+
+const app = http.createServer((req, res) => {
+  if (req.method == 'POST') {
+    let string = ''
+    req.on('data', (chunk) => {
+      string += chunk.toString()
+    })
+    req.on('end', () => {
+      const { url } = JSON.parse(string)
+
+      const fileName = sitemapGenrater(url);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ 'response': url }));
+    })
+  }
+})
+
+const server = app.listen(port, function () {
+  console.log("API listen at port 3000");
+});
+
+module.exports = server;
