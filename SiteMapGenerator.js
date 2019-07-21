4b252aa7794b451126bfff89f5cdfc7ca7659427
Rahul <rgupta@argolimited.com>
2019-07-21 16:57:19 +0530

    git push

--- SiteMapGenerator.js
+++ SiteMapGenerator.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const URL = require('url-parse');
+const axios = require('axios');
 
 const { stripTrailingSlash, linkValidator } = require('./utils');
 
@@ -49,26 +50,24 @@ class SiteMapGenerator {
   async visitPage(url) {
     this.pagesVisited[url] = true; //making sure page is visited
     this.numPagesVisited++;
-
-    return new Promise((resolve, reject) => {
-      request(url, (error, response, body) => {
-        // in addition to parsing the value, deal with possible errors
-        if (error) return reject(error);
-        try {
-          if (response.statusCode !== 200) {
-            resolve(this.crawl());
-          }
-          const $ = cheerio.load(body); // Parse the document body
-          this.getInternalLinks($);
-          resolve(this.crawl());
-        } catch (e) {
-          reject(e);
+    return axios
+      .get(url)
+      .then(response => {
+        if (response.status !== 200) {
+          console.log('not 200');
+          return this.crawl();
         }
+        const $ = cheerio.load(response.data); // Parse the document body
+        this.getPageLinks($);
+        return this.crawl();
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
       });
-    });
   }
 
-  getInternalLinks($) {
+  getPageLinks($) {
     const visitedList = Object.keys(this.pagesVisited);
 
     const links = $('a');
