613df9f7b5d51ffd5e67850497613723e1b9e563
Rahul <rgupta@argolimited.com>
2019-07-21 18:30:20 +0530

    ss

--- SiteMapGenerator.js
+++ SiteMapGenerator.js
@@ -52,18 +52,18 @@ class SiteMapGenerator {
   async visitPage(url) {
     this.pagesVisited[url] = true; //making sure page is visited
     this.numPagesVisited++;
+
     try {
-      return axios.get(url).then(async response => {
-        if (response.status !== 200) {
-          // may be page not found but keep crawling
-          return this.crawl();
-        }
-        const $ = cheerio.load(response.data); // Parse the document body
-        this.getPageLinks($);
+      const response = await axios.get(url);
+      if (response.status !== 200) {
+        // may be page not found but keep crawling
         return this.crawl();
-      });
+      }
+      const $ = cheerio.load(response.data); // Parse the document body
+      this.getPageLinks($);
+      return this.crawl();
     } catch (error) {
-      console.log('TCL: visitPage -> error', error);
+      console.log('error', error);
       return this.crawl();
     }
     // .catch(async error => {
